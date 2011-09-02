7b1c9cca36b6a1322a9e8c2dcdaf222efc4c7735
jsz <jszpilewski@me.com>
2011-09-02 12:53:53 +0200

    Initial Commit

---
+++ zazen/FXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FXAppDelegate.h
+//  zazen
+//
+//  Created by Jaroslaw Szpilewski on 2/9/11.
+//  Copyright (c) 2011 Flux Forge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (strong) IBOutlet NSWindow *window;
+
+@end
---
+++ zazen/FXAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  FXAppDelegate.m
+//  zazen
+//
+//  Created by Jaroslaw Szpilewski on 2/9/11.
+//  Copyright (c) 2011 Flux Forge. All rights reserved.
+//
+
+#import "FXAppDelegate.h"
+
+@implementation FXAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
